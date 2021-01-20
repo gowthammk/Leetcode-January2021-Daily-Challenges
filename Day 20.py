5a0d5d7a98f62c3c8992e168cca1cd4f9c1d31ba
Gowtham <mkgowtham7@gmail.com>
2021-01-20 09:30:53 +0000

    Valid Parentheses

---
+++ Day 20.py
@@ -0,0 +1,43 @@
+# Valid Parentheses
+#
+# Solution
+# Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
+# determine if the input string is valid.
+#
+# An input string is valid if:
+#
+# Open brackets must be closed by the same type of brackets.
+# Open brackets must be closed in the correct order.
+#
+#
+# Example 1:
+#
+# Input: s = "()"
+# Output: true
+# Example 2:
+#
+# Input: s = "()[]{}"
+# Output: true
+# Example 3:
+#
+# Input: s = "(]"
+# Output: false
+# Example 4:
+#
+# Input: s = "([)]"
+# Output: false
+# Example 5:
+#
+# Input: s = "{[]}"
+# Output: true
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) < 2 or len(s) % 2 != 0:
+            return False
+        while "()" in s or "{}" in s or "[]" in s:
+            s = s.replace("()", "").replace("{}", "").replace("[]", "")
+        if s == "":
+            return True
+        return False
