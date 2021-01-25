97c5aa07731e3b857d043f8ae2674b2658ea2e77
Gowtham <mkgowtham7@gmail.com>
2021-01-25 09:50:20 +0000

    Merge k Sorted Lists

---
+++ Day 24.py
@@ -0,0 +1,46 @@
+# Merge k Sorted Lists
+#
+# Solution
+# You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+#
+# Merge all the linked-lists into one sorted linked-list and return it.
+#
+#
+#
+# Example 1:
+#
+# Input: lists = [[1,4,5],[1,3,4],[2,6]]
+# Output: [1,1,2,3,4,4,5,6]
+# Explanation: The linked-lists are:
+# [
+#   1->4->5,
+#   1->3->4,
+#   2->6
+# ]
+# merging them into one sorted list:
+# 1->1->2->3->4->4->5->6
+# Example 2:
+#
+# Input: lists = []
+# Output: []
+# Example 3:
+#
+# Input: lists = [[]]
+# Output: []
+
+class Solution:
+    def mergeKLists(self, lists):
+        dummy = curr = ListNode(0)
+        heap = []
+        for ind, el in enumerate(lists):
+            if el: heappush(heap, (el.val, ind))
+
+        while heap:
+            val, ind = heappop(heap)
+            curr.next = ListNode(val)
+            curr = curr.next
+            if lists[ind].next:
+                lists[ind] = lists[ind].next
+                heappush(heap, (lists[ind].val, ind))
+
+        return dummy.next
